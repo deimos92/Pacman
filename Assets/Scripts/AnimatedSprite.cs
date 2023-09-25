4381c8ece5a68db843668e9dbfafc261758aa7ca
Stanislav Perekrest <deimos92@users.noreply.github.com>
2023-09-25 11:56:28 +0300

    Pacman animations + pellets mechanics

---
+++ Assets/Scripts/AnimatedSprite.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AnimatedSprite : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] sprites;
+    public float animationTime = 0.25f;
+    public int animationFrame {  get; private set; }
+    public bool isLooping = true;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Advance), animationTime, animationTime);
+    }
+
+    private void Advance()
+    {
+        if (!spriteRenderer.enabled)
+            return;
+
+        animationFrame++;
+
+        if (animationFrame >= sprites.Length && isLooping)
+        {
+            animationFrame = 0;
+        }
+
+        if (animationFrame >= 0 && animationFrame < sprites.Length)
+        {
+            spriteRenderer.sprite = sprites[animationFrame];
+        }
+    }
+
+    public void Restart()
+    {
+        animationFrame = -1;
+        Advance();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,137 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ghost[] ghosts;
+    public Pacman pacman;
+    public Transform pellets;
+
+    public int ghostMultiplier { get; private set; } = 1;
+
+    public int score {  get; private set; }
+    public int lives { get; private set; }
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (lives <= 0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        foreach (Transform pellet in pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+
+    private void ResetState()
+    {
+        ResetGhostMultiplier();
+
+        for (int i = 0; i < ghosts.Length; i++)
+        {
+            ghosts[i].gameObject.SetActive(true);
+        }
+
+        pacman.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < ghosts.Length; i++)
+        {
+            ghosts[i].gameObject.SetActive(false);
+        }
+
+        pacman.gameObject.SetActive(false);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public void GhostEaten(Ghost ghost)
+    {
+        int points = ghost.points * ghostMultiplier;
+        SetScore(score + points);
+        ghostMultiplier++;
+    }
+
+    public void PacmanEaten()
+    {
+        pacman.gameObject.SetActive(false);
+
+        SetLives(lives - 1);
+
+        if (lives > 0)
+        {
+            Invoke(nameof(ResetState), 3f);
+        }
+        else
+        {
+            GameOver();
+        }            
+    }
+
+    public void PelletEaten(Pellet pellet)
+    {
+        pellet.gameObject.SetActive(false);
+        SetScore(score + pellet.points);
+
+        if (!HasRemainingPellets())
+        {
+            pacman.gameObject.SetActive(false);
+            Invoke(nameof(NewRound), 3f);
+        }
+    }
+
+    public void PowerPelletEaten(PowerPellet pellet)
+    {
+        // TODO: change ghost state
+        
+        PelletEaten(pellet);
+        CancelInvoke();
+        Invoke(nameof(ResetGhostMultiplier), pellet.duration);
+    }
+
+    private bool HasRemainingPellets()
+    {
+        foreach (Transform pellet in pellets)
+        {
+            if (pellet.gameObject.activeSelf)
+                return true;
+        }
+
+        return false;
+    }
+
+    private void ResetGhostMultiplier()
+    {
+        ghostMultiplier = 1;
+    }
+}
